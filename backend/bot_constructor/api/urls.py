92cdefb7d794b746ddbaa1fcb21702635c7a1fd7
Vadim <vadim.n@cuttlesystems.com>
2022-10-27 19:06:27 +0600

    Начало описания API

--- backend/bot_constructor/api/urls.py
+++ backend/bot_constructor/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 
-from .views import first_endpoint
-
+from .views import first_endpoint, get_message
 
 urlpatterns = [
     path('first_endpoint/', first_endpoint, name='first_endpoint'),
+    path('message/id/<str:value>/', get_message, name='get_message')
 ]
--- backend/bot_constructor/api/views.py
+++ backend/bot_constructor/api/views.py
@@ -1,11 +1,22 @@
+import rest_framework.request
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
-def first_endpoint(request):
+def first_endpoint(request: rest_framework.request.Request):
     if request.method == 'GET':
         return Response({'message': 'Get запрос', 'data': request.data}, status=status.HTTP_200_OK)
     if request.method == 'POST':
         return Response({'message': 'Post запрос', 'data': request.data}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_message(request: rest_framework.request.Request, value: str):
+    assert isinstance(request, rest_framework.request.Request)
+    return Response(
+        {
+            'message_id': value
+        }
+    )
