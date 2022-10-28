82bd30face696b256849ed2e4d53b8a0ac522db9
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-10-28 10:18:38 +0600

    "Эндпоинт User"

--- backend/bot_constructor/api/urls.py
+++ backend/bot_constructor/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from .views import first_endpoint, get_message
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('first_endpoint/', first_endpoint, name='first_endpoint'),
     path('message/id/<str:value>/', get_message, name='get_message')
 ]
--- backend/bot_constructor/api/views.py
+++ backend/bot_constructor/api/views.py
@@ -2,7 +2,9 @@ import rest_framework.request
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
+from .serializers import User, UserSerializer
 
 @api_view(['GET', 'POST'])
 def first_endpoint(request: rest_framework.request.Request):
@@ -11,6 +13,10 @@ def first_endpoint(request: rest_framework.request.Request):
     if request.method == 'POST':
         return Response({'message': 'Post запрос', 'data': request.data}, status=status.HTTP_200_OK)
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 @api_view(['GET'])
 def get_message(request: rest_framework.request.Request, value: str):
