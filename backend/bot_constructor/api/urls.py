79c8328d1d114979ad2ec78b38361b19d8a9aa5c
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-10-31 13:06:32 +0600

    Созданы 2 эндпоинта messages и variants, доделаны модели, выполнены миграции, добавлено отображение полей в админке

--- backend/bot_constructor/api/urls.py
+++ backend/bot_constructor/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import first_endpoint, get_message, UserViewSet
+from .views import first_endpoint, get_message, UserViewSet, MessageViewSet, VariantViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'messages', MessageViewSet, basename='messages')
+router.register(r'variants', VariantViewSet, basename='variants')
 
 
 urlpatterns = [
--- backend/bot_constructor/bot_constructor/settings.py
+++ backend/bot_constructor/bot_constructor/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,11 +103,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
