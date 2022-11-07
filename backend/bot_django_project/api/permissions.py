40c225964a2505905559662a09ea9e8f260d26c4
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-11-07 11:18:13 +0600

    Исправлены замечания после ревью

--- backend/bot_django_project/api/permissions.py
+++ backend/bot_django_project/api/permissions.py
@@ -2,12 +2,25 @@ from rest_framework import permissions
 from bots.models import Message, Variant
 
 
-class IsBotOwnerOrForbidden(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Message):
-            return obj.bot.owner == request.user
-        elif isinstance(obj, Variant):
-            return obj.current_message.bot.owner == request.user
+class IsMessageOwnerOrForbidden(permissions.BasePermission):
+    """
+    Проверка получения доступа к объекту Message.
+    Разрешены сообщения только для своего бота.
+    """
+    def has_object_permission(self, request, view, obj: Message) -> bool:
+        return obj.bot.owner == request.user
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view) -> bool:
+        return request.user.is_authenticated
+
+
+class IsVariantOwnerOrForbidden(permissions.BasePermission):
+    """
+    Проверка получения доступа к объекту Variant.
+    Разрешены варианты только для своего бота.
+    """
+    def has_object_permission(self, request, view, obj: Variant) -> bool:
+        return obj.current_message.bot.owner == request.user
+
+    def has_permission(self, request, view) -> bool:
         return request.user.is_authenticated
