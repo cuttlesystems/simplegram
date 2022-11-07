67796e88cded1755fc9665bebdbe1ea327bb9e88
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-11-07 11:53:07 +0600

    permissions готовы

--- backend/bot_django_project/api/permissions.py
+++ backend/bot_django_project/api/permissions.py
@@ -1,5 +1,9 @@
 from rest_framework import permissions
+from rest_framework.request import Request
+from rest_framework.viewsets import ModelViewSet
+
 from bots.models import Message, Variant
+
 
 
 class IsMessageOwnerOrForbidden(permissions.BasePermission):
@@ -7,10 +11,10 @@ class IsMessageOwnerOrForbidden(permissions.BasePermission):
     Проверка получения доступа к объекту Message.
     Разрешены сообщения только для своего бота.
     """
-    def has_object_permission(self, request, view, obj: Message) -> bool:
+    def has_object_permission(self, request: Request, view: ModelViewSet, obj: Message) -> bool:
         return obj.bot.owner == request.user
 
-    def has_permission(self, request, view) -> bool:
+    def has_permission(self, request: Request, view: ModelViewSet) -> bool:
         return request.user.is_authenticated
 
 
@@ -19,8 +23,8 @@ class IsVariantOwnerOrForbidden(permissions.BasePermission):
     Проверка получения доступа к объекту Variant.
     Разрешены варианты только для своего бота.
     """
-    def has_object_permission(self, request, view, obj: Variant) -> bool:
+    def has_object_permission(self, request: Request, view: ModelViewSet, obj: Variant) -> bool:
         return obj.current_message.bot.owner == request.user
 
-    def has_permission(self, request, view) -> bool:
+    def has_permission(self, request: Request, view: ModelViewSet) -> bool:
         return request.user.is_authenticated
