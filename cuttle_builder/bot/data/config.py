0c8e8115ac3d716d514cb8b91db5a23969019228
Vadim <vadim.n@cuttlesystems.com>
2022-11-07 16:59:28 +0600

    Чистка кодогенерации. Монго не используется.

--- cuttle_builder/bot/data/config.py
+++ cuttle_builder/bot/data/config.py
@@ -1,4 +1,3 @@
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +7,3 @@ BOT_TOKEN = '5643702459:AAH8bWcmY9gTlM4nPXHMGMiax5RqvOdI08o'
 admins = [
     332783067
 ]
-
-USER = str(os.getenv('MONGO_USER'))
-PASSWORD = str(os.getenv('MONGO_PASSWORD'))
-IP = str(os.getenv('MONGO_IP'))
-
-MONGO_CONNECTION_KEY = f'mongodb+srv://{USER}:{PASSWORD}@{IP}/?retryWrites=true&w=majority'
