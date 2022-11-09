bcb500fd481b57d5a3486ea6ba2eb79f2b96acbe
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-11-09 16:34:52 +0600

    permissions for Bot

--- backend/bot_django_project/api/permissions.py
+++ backend/bot_django_project/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 
-from bots.models import Message, Variant
+from bots.models import Message, Variant, Bot
 
 
 
@@ -28,3 +28,17 @@ class IsVariantOwnerOrForbidden(permissions.BasePermission):
 
     def has_permission(self, request: Request, view: ModelViewSet) -> bool:
         return request.user.is_authenticated
+
+
+class IsBotOwnerOrForbidden(permissions.BasePermission):
+    """
+    Проверка получения доступа к объекту Bot.
+    Разрешен доступ только для своего бота.
+    """
+    def has_object_permission(self, request: Request, view: ModelViewSet, obj: Bot) -> bool:
+        print('has_object_permission')
+        return obj.owner == request.user
+
+    def has_permission(self, request: Request, view: ModelViewSet) -> bool:
+        print('has_permission')
+        return all(bot.owner == request.user for bot in view.get_queryset())
