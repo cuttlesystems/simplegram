c54dc82f0670a8b5ec2b3dc3075fdac101317b26
Vadim <vadim.n@cuttlesystems.com>
2022-11-11 17:13:52 +0600

    Точка отсчета путей для кодогенератора бота.

---
+++ cuttle_builder/bot_generator_params.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+CUTTLE_BUILDER_PATH = Path(__file__).parent
--- cuttle_builder/builder/keyboard_generator/create_reply_keyboard.py
+++ cuttle_builder/builder/keyboard_generator/create_reply_keyboard.py
@@ -1,11 +1,14 @@
 from b_logic.data_objects import MessageVariant
+from cuttle_builder.bot_generator_params import CUTTLE_BUILDER_PATH
 from cuttle_builder.builder.additional.file_read_write.read_file import read_file
 import typing
 from pathlib import Path
 
 
 def create_reply_keyboard(kb_name: str, buttons: typing.List[MessageVariant]) -> str:
-    reply_keyboard_sample = Path(__file__).parent.parent / 'additional/samples/reply_keyboard_sample.txt'
+    reply_keyboard_sample = (
+            CUTTLE_BUILDER_PATH / 'builder' / 'additional' /
+            'samples' / 'reply_keyboard_sample.txt')
     code = read_file(reply_keyboard_sample)
     code = code.format(
             kb_name,
