cf410abebfed0f0158082453000a9eafa47cd9f2
KanatSeitov <116721841+KanatSeitov@users.noreply.github.com>
2022-11-14 17:29:31 +0600

    Revert "add types and underscode, fix bug with not creating connection with b…"

--- cuttle_builder/bot/utils/set_bot_commands.py
+++ cuttle_builder/bot/utils/set_bot_commands.py
@@ -4,5 +4,11 @@ from aiogram import types, Dispatcher
 async def set_default_commands(dp: Dispatcher):
     await dp.bot.set_my_commands([
         types.BotCommand('start', 'Start bot'),
-        types.BotCommand('restart', 'Back to main menu')
+        types.BotCommand('update', 'Update profile'),
+        types.BotCommand('choose', 'Find pair')
+        # types.BotCommand('menu', 'Переход в главное меню'),
+        # types.BotCommand('registration', 'Начать регистрацию'),
+        # types.BotCommand('update', 'Обновить профиль'),
+        # types.BotCommand('match', 'Найти человека'),
+        # types.BotCommand('profile', 'Показать мой профиль')
     ])
