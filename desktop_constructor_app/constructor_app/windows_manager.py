ec7bec8d17ca754e6a1d62b35a660ba2b085d27a
Vadim <vadim.n@cuttlesystems.com>
2022-11-21 11:01:02 +0600

    Рефакторинг

---
+++ desktop_constructor_app/constructor_app/windows_manager.py
@@ -0,0 +1,33 @@
+from PySide6.QtCore import QObject
+
+from b_logic.bot_api import BotApi
+from b_logic.data_objects import BotDescription
+from desktop_constructor_app.constructor_app.bot_editor_form import BotEditorForm
+from desktop_constructor_app.constructor_app.login_form import LoginForm
+
+
+class WindowsManager(QObject):
+    def __init__(self):
+        super().__init__()
+        bot_api = BotApi()
+        self._login_form = LoginForm(None, bot_api)
+        self._bot_editor_form = BotEditorForm(None)
+
+        self._login_form.open_bot_signal.connect(self._on_open_bot)
+        self._bot_editor_form.close_bot.connect(self._on_close_bot)
+
+        self._login_form.show()
+
+    def start(self) -> None:
+        self._login_form.show()
+
+    def _on_open_bot(self, bot: BotDescription):
+        print('open bot', bot)
+        self._bot_editor_form.set_bot(bot)
+        self._login_form.hide()
+        self._bot_editor_form.show()
+
+    def _on_close_bot(self):
+        print('close bot')
+        self._bot_editor_form.hide()
+        self._login_form.show()
