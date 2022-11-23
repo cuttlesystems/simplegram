ce8cfc30621c3fc45c6ac669772f051fc254ccf8
Vadim <vadim.n@cuttlesystems.com>
2022-11-23 15:00:51 +0600

    Улучшенное задание размеров рабочей области, поддерживается скроллинг.

--- desktop_constructor_app/constructor_app/bot_scene.py
+++ desktop_constructor_app/constructor_app/bot_scene.py
@@ -23,19 +23,17 @@ class BotScene(QGraphicsScene):
 
         self._messages_rects: typing.List[QGraphicsRectItem] = []
 
-        self.add_message(10, 10)
+        self.add_message(100, 10)
 
-        self.add_message(10, 180)
+        self.add_message(100, 180)
 
         self.changed.connect(self._on_item_changed)
-
-        self._internal_scene_change = False
 
     def add_message(self, x: int, y: int):
         self._messages_rects.append(self._create_message(x, y))
 
-    def _get_bounding_rect(self) -> QRectF:
-        result = QRectF(0, 0, 0, 0)
+    def _get_work_field_rect(self) -> QRectF:
+        result = QRectF(0, 0, 640, 480)
         for message_rect in self._messages_rects:
             result = result.united(message_rect.sceneBoundingRect())
         return result
@@ -43,19 +41,20 @@ class BotScene(QGraphicsScene):
     def _on_item_changed(self, region):
         print('changed ', region)
 
-        if not self._internal_scene_change:
-            self._internal_scene_change = True
-            try:
-                items_bounding_rect: QRectF = self._get_bounding_rect()
-                x = items_bounding_rect.x() - 5
-                y = items_bounding_rect.y() - 5
-                width = items_bounding_rect.width() + 10
-                height = items_bounding_rect.height() + 10
-                background_rect_size = QRectF(x, y, width, height)
-                self._background_rect.setRect(background_rect_size)
-                print('background rect ', background_rect_size)
-            finally:
-                self._internal_scene_change = False
+        items_bounding_rect: QRectF = self._get_work_field_rect()
+        background_rect_size = self._extend_rect(items_bounding_rect, 50, 50)
+        self._background_rect.setRect(background_rect_size)
+        scene_rect_size = self._extend_rect(background_rect_size, 30, 30)
+        self.setSceneRect(scene_rect_size)
+        print('background rect ', background_rect_size)
+        print('scene rect ', scene_rect_size)
+
+    def _extend_rect(self, rect: QRectF, by_x: float, by_y: float) -> QRectF:
+        x = rect.x() - by_x
+        y = rect.y() - by_y
+        width = rect.width() + by_x * 2
+        height = rect.height() + by_y * 2
+        return QRectF(x, y, width, height)
 
     def _create_message(self, x: int, y: int) -> QGraphicsRectItem:
         rect: QGraphicsRectItem = self.addRect(
