96c8c55e99b8a364a6638703964c186e73858d1b
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-12-26 16:12:34 +0600

    qt/store user data in linux

--- desktop_constructor_app/constructor_app/utils/application_settings.py
+++ desktop_constructor_app/constructor_app/utils/application_settings.py
@@ -6,9 +6,11 @@ import platform
 def get_application_data_dir() -> Path:
     path: Path = None
     current_os = platform.system()
+    result: Path = None
     if current_os == "Linux":
-        path = Path('/var/lib')
+        path = Path.home()
+        result = path / '.cuttle_systems' / 'bot_constructor'
     elif current_os == "Windows":
         path = Path(os.getenv('APPDATA'))
-
-    return path / 'cuttle_systems' / 'bot_constructor'
+        result = path / 'cuttle_systems' / 'bot_constructor'
+    return result
