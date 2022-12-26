053bf6964b3f91bf1e4975de8ff289e1412da184
alexey-cuttle <aleksey.p@cuttlesystems.com>
2022-12-26 16:24:38 +0600

    fix

--- desktop_constructor_app/constructor_app/utils/application_settings.py
+++ desktop_constructor_app/constructor_app/utils/application_settings.py
@@ -1,12 +1,13 @@
 import os
+from typing import Optional
 from pathlib import Path
 import platform
 
 
 def get_application_data_dir() -> Path:
-    path: Path = None
+    path: Optional[Path] = None
+    result: Optional[Path] = None
     current_os = platform.system()
-    result: Path = None
     if current_os == "Linux":
         path = Path.home()
         result = path / '.cuttle_systems' / 'bot_constructor'
