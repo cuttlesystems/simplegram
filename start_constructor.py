2f4e164029cd3fbc4735123fc79433692067fc87
Vadim <vadim.n@cuttlesystems.com>
2023-01-05 18:22:33 +0600

    Попутно немного разобрался со стилями приложения

--- start_constructor.py
+++ start_constructor.py
@@ -1,10 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QStyleFactory
+
+from desktop_constructor_app.constructor_app.utils.application_style import print_available_styles
 from desktop_constructor_app.constructor_app.windows_manager import WindowsManager
 import desktop_constructor_app.constructor_app.rc_bot_icons
 
-
 if __name__ == "__main__":
+    print_available_styles()
+
     app = QApplication(sys.argv)
 
     # ссылаемся на модуль, чтобы он не удалился при автоматическом рефакторинге
