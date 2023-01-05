0f13ae026719dbc1d047d871f7ff2495d5af548c
Vadim <vadim.n@cuttlesystems.com>
2023-01-05 11:45:13 +0600

    Рефакторинг и комментарии.

--- desktop_constructor_app/constructor_app/graphic_scene/bot_view.py
+++ desktop_constructor_app/constructor_app/graphic_scene/bot_view.py
@@ -11,22 +11,39 @@ from desktop_constructor_app.constructor_app.graphic_scene.bot_scene import BotS
 
 
 class BotView(QGraphicsView):
+    """
+    Графический вид для отображения графической сцены бота
+    """
+
     def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
         assert isinstance(parent, Optional[QtWidgets.QWidget])
         super().__init__(parent)
 
-        # контекстное меню для места, где нет блока
-        self._context_menu_empty: typing.Optional[QMenu] = None
-
+        # контекстное меню для блока
         self._context_menu_block: typing.Optional[QMenu] = None
 
+        # контекстное меню пустого места
+        self._context_menu_empty: typing.Optional[QMenu] = None
+
+        # переменная хранит координаты точки откуда было вызвано контекстное меню
+        # (не None когда контекстное меню отображается)
         self._context_menu_position: typing.Optional[QPoint] = None
 
     def setup_empty_menu(self, context_menu: QMenu) -> None:
+        """
+        Установить контекстное меню, которое будет отображаться для пустого места
+        Args:
+            context_menu: меню
+        """
         assert isinstance(context_menu, QMenu)
         self._context_menu_empty = context_menu
 
     def setup_block_menu(self, context_menu: QMenu) -> None:
+        """
+        Установить контекстное меню, которое будет отображаться для блока
+        Args:
+            context_menu: меню
+        """
         assert isinstance(context_menu, QMenu)
         self._context_menu_block = context_menu
 
@@ -40,27 +57,39 @@ class BotView(QGraphicsView):
         return self._context_menu_position
 
     def get_central_point(self) -> QPoint:
+        """
+        Получить центральную точку виджета в относительных координатах виджета
+        Returns:
+            точка в относительных координатах виджета
+        """
         return QPoint(round(self.width() / 2), round(self.height() / 2))
 
     def contextMenuEvent(self, event: QtGui.QContextMenuEvent) -> None:
         assert isinstance(event.pos(), QPoint)
         self._context_menu_position: QPoint = event.pos()
         try:
+            # пересчитаем координаты события мыши в координаты сцены
             scene_pos: QPointF = self.mapToScene(self._context_menu_position)
+
             bot_scene: BotScene = typing.cast(BotScene, self.scene())
             assert isinstance(bot_scene, BotScene)
+
+            # найдем графический элемент сцены, который находится по координатам клика
             scene_item = bot_scene.itemAt(scene_pos, QTransform())
+
+            # если клик для вызова контекстного меню произведен по блоку
             if isinstance(scene_item, BlockGraphicsItem):
-                if scene_item in bot_scene.selectedItems():
-                    print('scene item in selected items')
-                    pass
-                else:
+
+                # если блок, по которому произведен клик не является частью текущего выделения,
+                # то сбросим существующее выделение и сформируем новое из одного этого блока
+                if scene_item not in bot_scene.selectedItems():
                     bot_scene.clearSelection()
                     scene_item.setSelected(True)
 
                 if self._context_menu_block is not None:
                     self._context_menu_block.exec(event.globalPos())
             else:
+                # если клик для вызова контекстного меню произведен вне блока
                 if self._context_menu_empty is not None:
                     self._context_menu_empty.exec(event.globalPos())
         finally:
