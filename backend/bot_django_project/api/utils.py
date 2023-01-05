1192eec26b68a68cd8e288b036d59e7125c6fce2
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-01-05 15:24:19 +0600

    Остановка всех запущенных ботов при перезапуске сервера

--- backend/bot_django_project/api/utils.py
+++ backend/bot_django_project/api/utils.py
@@ -1,5 +1,8 @@
 from api.exceptions import InvalidBotTokenWhenGenerateBot
+from b_logic.bot_processes_manager import BotProcessesManagerSingle
+from b_logic.bot_runner import BotRunner
 from bots.models import Bot
+from django.utils.autoreload import file_changed
 
 
 def check_bot_token_when_generate_bot(bot: Bot) -> None:
@@ -15,3 +18,26 @@ def check_bot_token_when_generate_bot(bot: Bot) -> None:
     assert isinstance(bot, Bot)
     if not bot.token:
         raise InvalidBotTokenWhenGenerateBot()
+
+
+def stop_all_running_bots_before_autoreload(sender, **kwargs) -> None:
+    """
+    Остановка всех запущенных ботов при перезапуске сервера.
+
+    Args:
+        sender: Отправитель сигнала. Обязательный аргумент.
+        **kwargs: Произвольные аргументы. Обязательный аргумент.
+        (https://docs.djangoproject.com/en/4.1/topics/signals/#receiver-functions)
+    """
+    print('before autoreload')
+    process_manager = BotProcessesManagerSingle()
+    all_running_processes = process_manager.get_all_processes_info()
+    if len(all_running_processes) > 0:
+        runner = BotRunner(None)
+        for process in all_running_processes.values():
+            runner.stop(process.process_id)
+
+
+# Сигнал file_changed испускается при обнаружении изменений в коде на запущенном сервере
+# После сигнала file_changed следует перезагрузка сервера
+file_changed.connect(stop_all_running_bots_before_autoreload)
