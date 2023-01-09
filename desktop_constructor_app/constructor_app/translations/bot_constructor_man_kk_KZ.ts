05dff96e9d83d0944a8a94e7624048ff7aaac13a
Vadim <vadim.n@cuttlesystems.com>
2023-01-09 11:35:59 +0600

    Ручные переводы для kz и en

---
+++ desktop_constructor_app/constructor_app/translations/bot_constructor_man_kk_KZ.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk_KZ">
+<context>
+    <name>LoginForm.manual</name>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Server address error</source>
+        <translation>Server address error</translation>
+    </message>
+    <message>
+        <source>Please fill in the server address field. For example: https://ramasuchka.kz/</source>
+        <translation>Please fill in the server address field. For example: https://ramasuchka.kz/</translation>
+    </message>
+</context>
+</TS>
