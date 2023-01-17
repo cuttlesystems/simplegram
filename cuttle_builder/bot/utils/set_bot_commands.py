44c3d20fee4a8b7c83cb49e06aaba4d9ce1f3e68
KanatSeytov <kseytov@gmail.com>
2023-01-17 10:08:17 +0600

    remove restart command

--- cuttle_builder/bot/utils/set_bot_commands.py
+++ cuttle_builder/bot/utils/set_bot_commands.py
@@ -3,12 +3,5 @@ from aiogram import types, Dispatcher
 
 async def set_default_commands(dp: Dispatcher):
     await dp.bot.set_my_commands([
-        types.BotCommand('start', 'Start bot'),
-        types.BotCommand('update', 'Update profile'),
-        types.BotCommand('choose', 'Find pair')
-        # types.BotCommand('menu', 'Переход в главное меню'),
-        # types.BotCommand('registration', 'Начать регистрацию'),
-        # types.BotCommand('update', 'Обновить профиль'),
-        # types.BotCommand('match', 'Найти человека'),
-        # types.BotCommand('profile', 'Показать мой профиль')
+        types.BotCommand('start', 'Start bot')
     ])
