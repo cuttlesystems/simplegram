dbad9ebc9974500d8ae850724a95d8ad948577dd
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-01-23 12:29:15 +0600

    change path to read git info file

--- mini_app/get_commit_info_from_github_api.py
+++ mini_app/get_commit_info_from_github_api.py
@@ -38,7 +38,6 @@ def get_commit_info_from_github_api() -> dict:
             date = date.replace(tzinfo=default_tz)
             date = date.astimezone(alma_tz)
             date = str(date).replace('+06:00', '')
-            result = f'Commit: {sha}, Author: {author}, Date: {date}'
             result = dict(commit_hash=sha, commit_author=author, commit_created_date=date)
         except KeyError as error:
             print(f'Commit doesn\'t have field: {error}')
