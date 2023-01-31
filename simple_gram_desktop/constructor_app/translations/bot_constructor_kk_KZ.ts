0bcbba505632dcd22e48fb7b2408caac920a755e
KanatSeytov <kseytov@gmail.com>
2023-01-31 14:05:02 +0600

    add kazakh translation

--- simple_gram_desktop/constructor_app/translations/bot_constructor_kk_KZ.ts
+++ simple_gram_desktop/constructor_app/translations/bot_constructor_kk_KZ.ts
@@ -6,93 +6,94 @@
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="14"/>
         <source>Simple Gram</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="33"/>
         <source>Bot stdout output</source>
-        <translation type="unfinished"></translation>
+        <translation>Бот  stdout output</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="56"/>
         <source>Bot stderr output</source>
-        <translation type="unfinished"></translation>
+        <translation>Бот stderr output</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="95"/>
         <source>Bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Бот</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="104"/>
         <source>Block</source>
-        <translation type="unfinished"></translation>
+        <translation>Блок</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="117"/>
         <source>Scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Схема</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="128"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Құралдар тақтасы</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="162"/>
         <source>Generate bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты жасау</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="171"/>
         <source>Start bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты қосу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="180"/>
         <source>Stop bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты тоқтату</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="189"/>
         <source>Add message</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>сообщение возможно поменять </translatorcomment>
+        <translation>Хабарламны қосу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="198"/>
         <source>Delete message</source>
-        <translation type="unfinished"></translation>
+        <translation>Хабарламаны жою</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="207"/>
         <source>Mark as start</source>
-        <translation type="unfinished"></translation>
+        <translation>Қосылды деп белгілеу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="216"/>
         <source>Delete variant</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқаны жою</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="221"/>
         <source>Manual save</source>
-        <translation type="unfinished"></translation>
+        <translation>Қолма қол сақтау</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="230"/>
         <source>Add variant</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқаны қосу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="239"/>
         <source>Read logs</source>
-        <translation type="unfinished"></translation>
+        <translation>Логтарды оқу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="248"/>
         <location filename="../widgets/bot_editor/bot_editor_form.ui" line="251"/>
         <source>Mark as error</source>
-        <translation type="unfinished"></translation>
+        <translation>Қате деп белгілеу</translation>
     </message>
 </context>
 <context>
@@ -100,67 +101,67 @@
     <message>
         <location filename="../widgets/login_form.ui" line="17"/>
         <source>Connection to Cuttle Systems Simple Gram</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuttle Systems Simple Gram-ға қосылу</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="25"/>
         <source>Server connection string</source>
-        <translation type="unfinished">Сервер қосылым жолы</translation>
+        <translation>Серверге қосылу сілтемесі</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="39"/>
         <source>Username</source>
-        <translation type="unfinished">Қолданушының аты</translation>
+        <translation>Қолданушының аты</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="49"/>
         <source>Password</source>
-        <translation type="unfinished">Құпия сөз</translation>
+        <translation>Құпия сөз</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="63"/>
         <source>Save my password</source>
-        <translation type="unfinished"></translation>
+        <translation>Құпия сөзді сақтау</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="74"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Қосылу</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="81"/>
         <source>Refresh list</source>
-        <translation type="unfinished"></translation>
+        <translation>Тізімді жаңарту</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="88"/>
         <source>Change user</source>
-        <translation type="unfinished"></translation>
+        <translation>Қолданушыны өзгерту</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="117"/>
         <source>Bots list</source>
-        <translation type="unfinished"></translation>
+        <translation>Боттар тізімі</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="177"/>
         <source>Open bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты ашу</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="184"/>
         <source>Create bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты жасау</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="191"/>
         <source>Remove bot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ботты жою</translation>
     </message>
     <message>
         <location filename="../widgets/login_form.ui" line="108"/>
         <source>Sign Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Тіркелу</translation>
     </message>
 </context>
 <context>
@@ -168,67 +169,67 @@
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="14"/>
         <source>Change message</source>
-        <translation type="unfinished"></translation>
+        <translation>Хабарламаны өзгерту</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="22"/>
         <source>Message text</source>
-        <translation type="unfinished"></translation>
+        <translation>Хабарлама</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="37"/>
         <source>Message type</source>
-        <translation type="unfinished"></translation>
+        <translation>Хабарлама түрі</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="43"/>
         <source>Message with variants</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқасы бар хабарлама</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="50"/>
         <source>Message with any input</source>
-        <translation type="unfinished"></translation>
+        <translation>Кез-келген енгізуді қабылдайтын хабарлама</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="57"/>
         <source>Message with jump</source>
-        <translation type="unfinished"></translation>
+        <translation>Секірмесі бар хабарлама</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="135"/>
         <source>Load image</source>
-        <translation type="unfinished"></translation>
+        <translation>Суретті көшіру</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="142"/>
         <source>Remove image</source>
-        <translation type="unfinished"></translation>
+        <translation>Суретті жою</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="153"/>
         <source>Keyboard type for variants</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқалар үшін пернетақта түрі</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="159"/>
         <source>Reply</source>
-        <translation type="unfinished"></translation>
+        <translation>Жауап</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="166"/>
         <source>Inline</source>
-        <translation type="unfinished"></translation>
+        <translation>Кезек</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="191"/>
         <source>Variable name</source>
-        <translation type="unfinished"></translation>
+        <translation>Айнымалы аты</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/message_editor_dialog.ui" line="203"/>
         <source>Next message</source>
-        <translation type="unfinished"></translation>
+        <translation>Келесі хабарлама</translation>
     </message>
 </context>
 <context>
@@ -236,52 +237,52 @@
     <message>
         <location filename="../widgets/sign_up_form.ui" line="20"/>
         <source>Sign Up Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Тіркелу формасы</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="28"/>
         <source>Email</source>
-        <translation type="unfinished"></translation>
+        <translation>Email</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="41"/>
         <source>Confirm your password here ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Құпия сөзді растау...</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="48"/>
         <source>Password</source>
-        <translation type="unfinished">Құпия сөз</translation>
+        <translation>Құпия сөз</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="55"/>
         <source>Enter your email address here ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Өзіңіздің email-ыңызды енгізіңіз...</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="62"/>
         <source>Enter your username here ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Пайдаланушы атын енгізіңіз...</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="69"/>
         <source>Username</source>
-        <translation type="unfinished">Қолданушының аты</translation>
+        <translation>Қолданушының аты</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="76"/>
         <source>Confirm Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Құпия сөзді растау</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="89"/>
         <source>Enter your password here ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Құпия сөзді енгізіңіз...</translation>
     </message>
     <message>
         <location filename="../widgets/sign_up_form.ui" line="126"/>
         <source>Sign up</source>
-        <translation type="unfinished"></translation>
+        <translation>Тіркелу</translation>
     </message>
 </context>
 <context>
@@ -289,17 +290,17 @@
     <message>
         <location filename="../widgets/bot_editor/variant_editor_dialog.ui" line="14"/>
         <source>Variant Editor Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқаны өңдеу</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/variant_editor_dialog.ui" line="22"/>
         <source>Variant (button) text</source>
-        <translation type="unfinished"></translation>
+        <translation>Нұсқа (батырма)</translation>
     </message>
     <message>
         <location filename="../widgets/bot_editor/variant_editor_dialog.ui" line="36"/>
         <source>On select this variant go to message</source>
-        <translation type="unfinished"></translation>
+        <translation>Осы нұсқаны таңдаудан хабарламаға өту</translation>
     </message>
 </context>
 </TS>
--- simple_gram_desktop/constructor_app/translations/bot_constructor_man_kk_KZ.ts
+++ simple_gram_desktop/constructor_app/translations/bot_constructor_man_kk_KZ.ts
@@ -5,19 +5,19 @@
     <name>LoginForm.manual</name>
     <message>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>Қате</translation>
     </message>
     <message>
         <source>Server address error</source>
-        <translation>Server address error</translation>
+        <translation>Сервер сілтемесінен қате</translation>
     </message>
     <message>
         <source>Please fill in the server address field. For example: https://ramasuchka.kz/</source>
-        <translation>Please fill in the server address field. For example: https://ramasuchka.kz/</translation>
-    </message>   
+        <translation>Өтініш, сервер сілтемесін толтырыңыз. Мысалы: https://ramasuchka.kz/</translation>
+    </message>
     <message>
         <source>Bot creation error: {0}</source>
-        <translation>Bot creation error: {0}</translation>
-    </message>        
+        <translation>Бот жасаудан қате: {0}</translation>
+    </message>
 </context>
 </TS>
