8bbade550396603ce22d89dac7c5d0b12f9f4e0b
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-02-03 16:59:55 +0600

    добавлено поле profile_photo в модель бота + миграции

--- server_simple_gram/backend/bot_django_project/api/views.py
+++ server_simple_gram/backend/bot_django_project/api/views.py
@@ -21,7 +21,7 @@ from rest_framework.exceptions import ValidationError
 from .utils import check_bot_token_when_generate_bot
 from cuttle_builder.bot_generator_db import BotGeneratorDb
 from cuttle_builder.exceptions.bot_gen_exceptions import BotGeneratorException
-from .serializers import (BotSerializer, OneBotSerializer, MessageSerializer, MessageSerializerWithVariants,
+from .serializers import (BotSerializer, BotSerializerWithBotLink, MessageSerializer, MessageSerializerWithVariants,
                           VariantSerializer, CommandSerializer)
 from bots.models import Bot, Message, Variant, Command
 from .mixins import RetrieveUpdateDestroyViewSet
@@ -59,7 +59,7 @@ class BotViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         if self.request.query_params.get(BOT_WITH_LINK) == '1':
-            return OneBotSerializer
+            return BotSerializerWithBotLink
         return BotSerializer
 
     def get_queryset(self) -> QuerySet:
