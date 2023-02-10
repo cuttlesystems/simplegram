04d1e789d6ced502ef561ed8ca5238e78db99bf3
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-02-10 19:01:20 +0600

    добавление и отображение работает

--- simple_gram_desktop/constructor_app/widgets/selected_project_widget.py
+++ simple_gram_desktop/constructor_app/widgets/selected_project_widget.py
@@ -78,7 +78,8 @@ class SelectedProjectWidget(QWidget):
         # Set name bot in lineEdit
         assert isinstance(bot, BotDescription)
         assert isinstance(bot_state, bool)
-        self._bot = bot
+        # обновляем информацию о боте для корректного отображения картинки
+        self._bot = self._bot_api.get_bot_by_id(bot.id)
 
         # Установка дефолтной аватарки бота или фотки из БД, если есть.
         if self._bot.bot_profile_photo is not None:
@@ -115,9 +116,6 @@ class SelectedProjectWidget(QWidget):
             self._bot.bot_profile_photo = full_path_to_file
             self._bot.profile_photo_filename = os.path.basename(full_path_to_file)
             self._bot_api.change_bot(self._bot)
-
-            # обновляем информацию о боте для корректного отображения картинки
-            self._bot = self._bot_api.get_bot_by_id(self._bot.id)
 
             # отправляем сигнал на обновление бот_листа
             self.bot_avatar_changed_signal.emit()
