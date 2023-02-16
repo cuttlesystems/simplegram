bf38926fb4f7767c5bfe4b57bc3d84cc6c211e0c
Vadim <vadim.n@cuttlesystems.com>
2023-02-16 21:40:54 +0600

    Качество кода

--- server_simple_gram/backend/bot_django_project/api/apps.py
+++ server_simple_gram/backend/bot_django_project/api/apps.py
@@ -9,7 +9,7 @@ class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
-    def ready(self):
+    def ready(self) -> None:
         if os.environ.get('RUN_MAIN'):
             from bots.started_bots_managing.restart_bots_manage import start_all_launched_bots
             logger_django.info_logging('Django is started. Call autorun bots')
