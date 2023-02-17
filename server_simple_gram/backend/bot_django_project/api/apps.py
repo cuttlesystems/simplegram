c5546192dec2f0b75d1ab8cfa8e1dfebd204cc0b
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-02-17 12:52:08 +0600

    check gunicorn

--- server_simple_gram/backend/bot_django_project/api/apps.py
+++ server_simple_gram/backend/bot_django_project/api/apps.py
@@ -3,6 +3,8 @@ import sys
 
 from django.apps import AppConfig
 
+from api.check_guinicorn import is_run_with_gunicorn
+
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -11,7 +13,13 @@ class ApiConfig(AppConfig):
     def ready(self) -> None:
         from bot_constructor.log_configs import logger_django
         logger_django.info_logging('Method "ready" is activated')
-        if os.environ.get('RUN_MAIN'):
+        execute_code = False
+        if is_run_with_gunicorn():
+            execute_code = True
+        else:
+            if os.environ.get('RUN_MAIN'):
+                execute_code = True
+        if execute_code:
             from bots.started_bots_managing.restart_bots_manage import start_all_launched_bots
             logger_django.info_logging('Django is started. Call autorun bots')
             start_all_launched_bots()
