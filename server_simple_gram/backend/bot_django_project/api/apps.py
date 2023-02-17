5533de67003187993f3be3cf5d77cef2e91ef75d
Vadim <vadim.n@cuttlesystems.com>
2023-02-17 10:22:44 +0600

    Добавил логи и настройку default_app_config

--- server_simple_gram/backend/bot_django_project/api/apps.py
+++ server_simple_gram/backend/bot_django_project/api/apps.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 from django.apps import AppConfig
-from bot_constructor.log_configs import logger_django
 
 
 class ApiConfig(AppConfig):
@@ -10,6 +9,8 @@ class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self) -> None:
+        from bot_constructor.log_configs import logger_django
+        logger_django.info_logging('Method "ready" is activated')
         if os.environ.get('RUN_MAIN'):
             from bots.started_bots_managing.restart_bots_manage import start_all_launched_bots
             logger_django.info_logging('Django is started. Call autorun bots')
