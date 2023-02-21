a996e2bca6960d4a122e9d48b8236300d2e6dc2c
Islam <islam.k@cuttlesystems.com>
2023-02-21 15:55:24 +0600

    Исправлена генерация ботов с картинками

--- cuttle_builder/bot_generator_db.py
+++ cuttle_builder/bot_generator_db.py
@@ -1,7 +1,8 @@
+import typing
 from pathlib import Path
 
 from b_logic.bot_api.i_bot_api import IBotApi
-from b_logic.data_objects import BotDescription
+from b_logic.data_objects import BotDescription, BotMessage
 from cuttle_builder.bot_generator import BotGenerator
 
 
@@ -10,6 +11,8 @@ class BotGeneratorDb(BotGenerator):
         assert isinstance(bot_api, IBotApi)
         assert isinstance(bot, BotDescription)
 
+        self._bot_api = bot_api
+        
         commands = bot_api.get_commands(bot)
         messages = bot_api.get_messages(bot)
 
@@ -19,9 +22,18 @@ class BotGeneratorDb(BotGenerator):
             message_variants = bot_api.get_variants(mes)
             all_variants.extend(message_variants)
         print(bot.start_message_id)
+
+        self._preprocess_data(messages)
+
         super().__init__(
             messages=messages,
             variants=all_variants,
             commands=commands,
             bot=bot,
             bot_path=bot_dir)
+
+    def _preprocess_data(self, messages: typing.List[BotMessage]):
+        pass
+
+    def _after_finished(self):
+        pass
