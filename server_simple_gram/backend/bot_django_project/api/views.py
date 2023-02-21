df44cc91e3ec87d1f10d5a3a06e0d661e43da18b
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-02-21 14:46:32 +0600

    остановка и удаление директории с кодом бота

--- server_simple_gram/backend/bot_django_project/api/views.py
+++ server_simple_gram/backend/bot_django_project/api/views.py
@@ -92,6 +92,12 @@ class BotViewSet(viewsets.ModelViewSet):
         serializer.save()
         set_bot_must_be_generated_value(bot, True)
 
+    def perform_destroy(self, instance: Bot):
+        self._stop_bot_if_it_run(instance.id)
+        bot_dir = self._get_bot_dir(instance.id)
+        shutil.rmtree(path=bot_dir, ignore_errors=True)
+        instance.delete()
+
     def _get_bot_dir(self, bot_id: int) -> Path:
         assert isinstance(bot_id, int)
         bot_dir = BOTS_DIR / f'bot_{bot_id}'
