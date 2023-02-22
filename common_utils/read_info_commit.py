b04a5a8c2ecf9ac206c02cfa0b076bda6da53f9f
Shamil <shamil.s@cuttlesystems.com>
2023-02-22 16:48:52 +0600

    Deleted unneeded fixes from 'read_info_commit.py'

--- common_utils/read_info_commit.py
+++ common_utils/read_info_commit.py
@@ -12,22 +12,8 @@ def read_info_from_file_about_commit() -> str:
         str: Данные о коммите
     """
     result = 'Information about commit not found'
-    # при deploy'е через bash-скрипт
-    commit_info_through_bash_json_file_path = get_project_root_dir() / 'mini_app' / 'current_commit_info.json'
-    # при deploy'е через python-скрипт
-    commit_info_through_python_json_file_path = (
-        get_project_root_dir().parent / 'deploy_folder' / 'tg_bot_constructor' / 'mini_app' / 'current_commit_info.json'
-    )
-    if Path(commit_info_through_bash_json_file_path).exists():
-        filename = str(commit_info_through_bash_json_file_path)
-        with open(filename, 'r') as commit_info:
-            commit_dict = json.load(commit_info)
-            if 'commit_hash' in commit_dict:
-                result = ('Commit hash: ' + commit_dict['commit_hash'] +
-                          ', Author: ' + commit_dict['commit_author'] +
-                          ', Commit date: ' + commit_dict['commit_created_date'])
-    elif Path(commit_info_through_python_json_file_path).exists():
-        filename = str(commit_info_through_python_json_file_path)
+    filename = str(get_project_root_dir() / 'mini_app' / 'current_commit_info.json')
+    if Path(filename).exists():
         with open(filename, 'r') as commit_info:
             commit_dict = json.load(commit_info)
             if 'commit_hash' in commit_dict:
