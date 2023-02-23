7a7858a9dc7b7984f3c2acc9fda52e7b00864cfe
Shamil <shamil.s@cuttlesystems.com>
2023-02-23 16:45:21 +0600

    Revert 'def get_project_root_dir()' deletion

--- common_utils/get_root_dir.py
+++ common_utils/get_root_dir.py
@@ -1,13 +1,23 @@
 from pathlib import Path
 
 
+def get_project_root_dir() -> Path:
+    """
+    Возвращает полный путь до корневой директории проекта
+
+    Returns: Полный путь до корневой директории проекта
+    """
+    print('Do not use \'get_project_root_dir_name\' from \'common_utils\'. Refactor')
+    return Path(__file__).resolve().parent.parent
+
+
 def get_project_root_dir_name() -> str:
     """
     Возвращает имя корневой директории проекта.
 
     Returns: Имя корневой директории проекта.
     """
-    print('Don not use get_project_root_dir_name from common_utils. Refactor')
+    print('Do not use \'get_project_root_dir_name\' from \'common_utils\'. Refactor')
     root_path = str(get_project_root_dir())
     root_path_list = root_path.split('/')
     return root_path_list[-1]
