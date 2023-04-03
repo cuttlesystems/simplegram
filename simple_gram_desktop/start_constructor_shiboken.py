3fe7530772f15b1e8b46373b75f1dd6231c58654
GalikSergei <123621380+GalikSergei@users.noreply.github.com>
2023-04-03 13:18:10 +0600

    Update start_constructor_shiboken.py

--- simple_gram_desktop/start_constructor_shiboken.py
+++ simple_gram_desktop/start_constructor_shiboken.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication
 from utils.compile_resources import compile_all_resources
 
 def main():
+
     compile_all_resources()
 
     # и потом можно выполнить импорты, поскольку они могут появиться после компиляции
